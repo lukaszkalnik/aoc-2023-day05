3c64d6bd301410eb1dee2ed962bcce9e47e79dd9
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-12-16 20:55:42 +0100

    Solve part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,37 @@
 package org.example
 
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    val input = File("input.txt").readLines()
+
+    val sliceIndices = input.flatMapIndexed { index, line ->
+        when {
+            index == 0 || index == input.lastIndex -> listOf(index)
+            line.isEmpty() -> listOf(index - 1, index + 2)
+            else -> emptyList()
+        }
+    }.windowed(size = 2, step = 2)
+
+    val seedsAndMaps = sliceIndices.map { (from, to) -> input.slice(from..to) }
+
+    var seeds = seedsAndMaps[0][0].split(": ")[1].split(" ").map { it.toLong() }
+
+    seedsAndMaps.drop(1).map {
+        it.map { line ->
+            line.split(" ").map { it.toLong() }
+        }
+    }.forEach { transformationMap ->
+        seeds = seeds.map { it.transform(transformationMap) }
+    }
+
+    println(seeds.min())
 }
+
+fun Long.transform(transformationMap: List<List<Long>>): Long =
+    transformationMap.find { (_, sourceRangeStart, rangeLength) ->
+        this in sourceRangeStart..<sourceRangeStart + rangeLength
+    }?.let { (destinationRangeStart, sourceRangeStart, _) ->
+        val offset = this - sourceRangeStart
+        destinationRangeStart + offset
+    } ?: this
